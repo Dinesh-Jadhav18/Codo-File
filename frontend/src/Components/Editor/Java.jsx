06b993005e1b68ded67813678c96ea4491164dca
Dinesh <dineshbjadhav2003@gmail.com>
2026-09-09 14:50:34 +0530

    Update backend API URLs for deployment

--- frontend/src/Components/Editor/Java.jsx
+++ frontend/src/Components/Editor/Java.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
-import LangList from './LangList'
+import React, { useState } from "react";
+import LangList from "./LangList";
 // import voice from '../../assets/image.png'
-import axios from 'axios';
-import { toast } from 'react-hot-toast';
-import copy_icon from '../../assets/copy_icon.gif';
-import download_icon from '../../assets/download_logo.png';
-
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import copy_icon from "../../assets/copy_icon.gif";
+import download_icon from "../../assets/download_logo.png";
 
 function Java() {
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
 
-  const [code,setCode] = useState('')
-  const [output,setOutput] = useState('');
-
-  const handleSubmit = async ()=>{
-    toast.loading('Please Wait while File is executing')
+  const handleSubmit = async () => {
+    toast.loading("Please Wait while File is executing");
     const payload = {
-      language:"dart",
-      code
+      language: "dart",
+      code,
     };
- 
-    try{
+
+    try {
       // const {data} = await axios.post("https://codo-file-backend-76cx.onrender.com/rundart",payload)
-      const response = await fetch("https://codo-file-backend-76cx.onrender.com/rundart",{
-        method:'POST',
-        headers:{
-          "Content-Type":"application/json"
-          },  
-          body : JSON.stringify(payload),
-      })
-      const data = await response.json()
-      if(response.ok){
+      const response = await fetch(
+        "https://codo-file-backend-76cx.onrender.com/rundart",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        },
+      );
+      const data = await response.json();
+      if (response.ok) {
         toast.remove();
         setOutput(data.output);
-        toast.success("Executed  Successfully")
-      }
-      else{
+        toast.success("Executed  Successfully");
+      } else {
         setOutput(data.error);
         toast.remove();
         toast.error("An Error Occured");
@@ -43,81 +43,109 @@ function Java() {
       // toast.remove();
       // toast.success('Executed Successfully.')
       // setCode("");
-    }catch(err){
+    } catch (err) {
       toast.remove();
-      toast.error('Error in communication with the server.Please check is flutter sdk is installed');
+      toast.error(
+        "Error in communication with the server.Please check is flutter sdk is installed",
+      );
       setOutput("Error in communication with the server");
       console.log(`error is in dart.js .The error : ${err}`);
     }
-  }
+  };
 
-  const clear = ()=>{
-    toast.success('Output Cleared')
+  const clear = () => {
+    toast.success("Output Cleared");
     const box = document.querySelector(".rightplayground p");
     box.innerHTML = "";
-  }
+  };
 
-  const copyContent = ()=>{
-    toast.success('Copied')
+  const copyContent = () => {
+    toast.success("Copied");
     navigator.clipboard.writeText(code);
-  }
+  };
 
-  const codeToFile = ()=>{
-
+  const codeToFile = () => {
     // const text = document.querySelector('.codemirror').value;
-    const text = document.querySelector('#dart').value;
+    const text = document.querySelector("#dart").value;
     // const text = setcode(code);
-    const blob = new Blob([text],{type:"text/dart"});
+    const blob = new Blob([text], { type: "text/dart" });
 
     const link = document.createElement("a");
     link.href = window.URL.createObjectURL(blob);
     link.download = "codofile-dart.dart";
-    toast.success('File is Downloading...');
+    toast.success("File is Downloading...");
     link.click();
-  }
+  };
 
-  return ( 
+  return (
     <>
       <div className="voiceContainer">
-            <div className="voiceBody wholeeditorBody">
-                <div className="leftLang">
-                    <LangList leftcolordart="white"/>
+        <div className="voiceBody wholeeditorBody">
+          <div className="leftLang">
+            <LangList leftcolordart="white" />
+          </div>
+          <div className="PlaygroundMain">
+            <div className="runHeaderJS">
+              <div className="jsleftheaderfile jsfile">
+                <mark>
+                  <h2>index.dart</h2>
+                </mark>
+                <div className="runbtn">
+                  <button className="vbtn">
+                    <img
+                      className="voicebtn"
+                      onClick={copyContent}
+                      src={copy_icon}
+                      alt="voice"
+                    />
+                  </button>
+                  <button className="vbtn">
+                    <img
+                      className="voicebtn"
+                      onClick={codeToFile}
+                      src={download_icon}
+                      alt="voice"
+                    />
+                  </button>
+                  <button className="btn" onClick={handleSubmit}>
+                    RUN
+                  </button>
                 </div>
-                <div className="PlaygroundMain">
-                <div className='runHeaderJS'>
-                    <div className='jsleftheaderfile jsfile'>
-                      <mark><h2>index.dart</h2></mark>
-                      <div className='runbtn'>
-                      <button className='vbtn'>
-                      <img className='voicebtn' onClick={copyContent} src={copy_icon} alt='voice'/>
-                      </button>
-                      <button className='vbtn'>
-                      <img className='voicebtn' onClick={codeToFile} src={download_icon} alt='voice'/>
-                      </button>
-                        <button className='btn' onClick={handleSubmit}>RUN</button>
-                      </div>
-                    </div>
-                    <div className='jsrightheaderfile jsfile'>
-                      <mark><p>OUTPUT</p></mark>
-                      <button className='clear' onClick={clear}>Clear</button>
-                    </div>
-                  </div>
-                  <div className='jsplayground playground'>
-                    <div className='leftplayground snippet'>
-                      <textarea className='dartpython' name="dart" id="dart" value={code} onChange={(e)=>{setCode(e.target.value)}} placeholder='void main(){print("Hello codoPlayers");}'></textarea>
-                    </div>
-                    <h1 className="invisible">
-                      <mark>Output</mark>
-                    </h1>
-                    <div className='rightplayground snippet'>
-                      <p>{output}</p>
-                    </div>
-                  </div>
-                </div>
+              </div>
+              <div className="jsrightheaderfile jsfile">
+                <mark>
+                  <p>OUTPUT</p>
+                </mark>
+                <button className="clear" onClick={clear}>
+                  Clear
+                </button>
+              </div>
             </div>
+            <div className="jsplayground playground">
+              <div className="leftplayground snippet">
+                <textarea
+                  className="dartpython"
+                  name="dart"
+                  id="dart"
+                  value={code}
+                  onChange={(e) => {
+                    setCode(e.target.value);
+                  }}
+                  placeholder='void main(){print("Hello codoPlayers");}'
+                ></textarea>
+              </div>
+              <h1 className="invisible">
+                <mark>Output</mark>
+              </h1>
+              <div className="rightplayground snippet">
+                <p>{output}</p>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Java
+export default Java;
--- frontend/src/Components/Screens/Logout.jsx
+++ frontend/src/Components/Screens/Logout.jsx
@@ -1,39 +1,41 @@
-import React, { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import {UsedContext} from '../App'
-
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { UsedContext } from "../App";
 
 function Logout() {
+  const { state, dispatch } = useContext(UsedContext);
 
-    const {state,dispatch} = useContext(UsedContext);
+  const history3 = useNavigate();
 
-    const history3 = useNavigate()
-
-    useEffect(()=>{
-        fetch('https://codo-file-backend-76cx.onrender.com/logout',{
-            method:'GET',
-            headers:{
-                "Content-Type":"application/json",
-                Accept: "application/json",
-            },
-            credentials:"include"
-        }).then((res)=>{
-            dispatch({type:'USER',payload:false});
-            history3("/login")
-            if(!res.status === 200){
-                const error = new Error(res.Error)
-                throw error;
-            }
-        }).catch((err)=>{
-            console.log("error in logout", err)
-            dispatch({type:'USER',payload:false});
-            history3("/login")
-        })
+  useEffect(() => {
+    fetch("https://codo-file-backend-76cx.onrender.com/logout", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
     })
+      .then((res) => {
+        dispatch({ type: "USER", payload: false });
+        history3("/login");
+        if (!res.status === 200) {
+          const error = new Error(res.Error);
+          throw error;
+        }
+      })
+      .catch((err) => {
+        console.log("error in logout", err);
+        dispatch({ type: "USER", payload: false });
+        history3("/login");
+      });
+  });
 
   return (
-    <mark><h1>Logout</h1></mark>
-  )
+    <mark>
+      <h1>Logout</h1>
+    </mark>
+  );
 }
 
-export default Logout
+export default Logout;
